bazurbat/chicken-scheme

commit 9247cec3930849e6570a18333739b9733daed2ce

Reimplement topological-sort with cycle detection.
Signed-off-by: felix <felix@call-with-current-continuation.org>

data-structures.scm
@@ -707,52 +707,49 @@
 	(sort! (append seq '()) less?)))
 
 
-;;;  Simple topological sort:
-;
-; Taken from SLIB (slightly adapted): Copyright (C) 1995 Mikael Djurfeldt
+;;; Topological sort with cycle detection:
+;;
+;; A functional implementation of the algorithm described in Cormen,
+;; et al. (2009), Introduction to Algorithms (3rd ed.), pp. 612-615.
 
 (define (topological-sort dag pred)
-  (if (null? dag)
-      '()
-      (let* ((adj-table '())
-	     (sorted '()))
-
-	(define (insert x y)
-	  (let loop ([at adj-table])
-	    (cond [(null? at) (set! adj-table (cons (cons x y) adj-table))]
-		  [(pred x (caar at)) (set-cdr! (car at) y)]
-		  [else (loop (cdr at))] ) ) )
-	
-	(define (lookup x)
-	  (let loop ([at adj-table])
-	    (cond [(null? at) #f]
-		  [(pred x (caar at)) (cdar at)]
-		  [else (loop (cdr at))] ) ) )
-	
-	(define (visit u adj-list)
-	  ;; Color vertex u
-	  (insert u 'colored)
-	  ;; Visit uncolored vertices which u connects to
-	  (for-each (lambda (v)
-		      (let ((val (lookup v)))
-			(if (not (eq? val 'colored))
-			    (visit v (or val '())))))
-		    adj-list)
-	  ;; Since all vertices downstream u are visited
-	  ;; by now, we can safely put u on the output list
-	  (set! sorted (cons u sorted)) )
-	
-	;; Hash adjacency lists
-	(for-each (lambda (def) (insert (car def) (cdr def)))
-		  (cdr dag))
-	;; Visit vertices
-	(visit (caar dag) (cdar dag))
-	(for-each (lambda (def)
-		    (let ((val (lookup (car def))))
-		      (if (not (eq? val 'colored))
-			  (visit (car def) (cdr def)))))
-		  (cdr dag)) 
-	sorted) ) )
+  (define (visit dag node edges path state)
+    (case (alist-ref node (car state) pred)
+      ((grey)
+       (##sys#abort
+        (##sys#make-structure
+         'condition
+         '(exn runtime cycle)
+         `((exn . message) "cycle detected"
+           (exn . arguments) ,(list (cons node (reverse path)))
+           (exn . call-chain) ,(##sys#get-call-chain)
+           (exn . location) topological-sort))))
+      ((black)
+       state)
+      (else
+       (let walk ((edges (or edges (alist-ref node dag pred '())))
+                  (state (cons (cons (cons node 'grey) (car state))
+                               (cdr state))))
+         (if (null? edges)
+             (cons (alist-update! node 'black (car state) pred)
+                   (cons node (cdr state)))
+             (let ((edge (car edges)))
+               (walk (cdr edges)
+                     (visit dag
+                            edge
+                            #f
+                            (cons edge path)
+                            state))))))))
+  (let loop ((dag dag)
+             (state (cons (list) (list))))
+    (if (null? dag)
+        (cdr state)
+        (loop (cdr dag)
+              (visit dag
+                     (caar dag)
+                     (cdar dag)
+                     '()
+                     state)))))
 
 
 ;;; Binary search:

tests/data-structures-tests.scm
@@ -2,6 +2,11 @@
 
 (use data-structures)
 
+(define-syntax assert-error
+  (syntax-rules ()
+    ((_ expr)
+     (assert (handle-exceptions _ #t expr #f)))))
+
 (let ((alist '((foo . 123) ("bar" . "baz"))))
   (alist-update! 'foo 999 alist)
   (assert (= (alist-ref 'foo alist) 999))
@@ -37,3 +42,10 @@
 (assert (> 0 (string-compare3-ci "foo\x00A" "foo\x00b")))
 (assert (< 0 (string-compare3-ci "foo\x00b" "foo\x00a")))
 (assert (< 0 (string-compare3-ci "foo\x00b" "foo\x00A")))
+
+;; topological-sort
+
+(assert (equal? '() (topological-sort '() eq?)))
+(assert (equal? '(a b c d) (topological-sort '((a b) (b c) (c d)) eq?)))
+(assert (equal? '(c d a b) (topological-sort '((a b) (c d)) eq?)))
+(assert-error (topological-sort '((a b) (b a)) eq?))

manual/Unit data-structures
@@ -282,6 +282,9 @@ Time complexity: O (|V| + |E|)
 (socks undershorts pants shoes watch shirt belt tie jacket)
 </enscript>
 
+If a cycle is detected during the sorting process, an exception of the
+condition kinds {{(exn runtime cycle)}} is thrown.
+
 === Random numbers
 
 

kdltr/chicken-core

commit 8ff9dffa92707b4846ea7f6df0e3e260d3df3d9a

Fix data corruption problem in list->queue and add a set of tests for queues.
Signed-off-by: Mario Domenech Goulart <mario.goulart@gmail.com>
(cherry picked from commit 5bf6208bd5e1110bf55c8cd161079d7dad693579)

Conflicts:
	tests/data-structures-tests.scm

Note: This depends on assert-error introduced in 9247cec393084.

data-structures.scm
@@ -866,7 +866,8 @@
 	   ((eq? (##sys#slot lst 1) '()) lst)
 	 (if (or (not (##core#inline "C_blockp" lst))
 		 (not (##core#inline "C_pairp" lst)) )
-	     (##sys#error-not-a-proper-list lst0 'list->queue) ) ) ) ) )
+	     (##sys#error-not-a-proper-list lst0 'list->queue) ) ) )
+   (##sys#length lst0)) )
 
 
 ; (queue-push-back! queue item)

tests/data-structures-tests.scm
@@ -49,3 +49,117 @@
 (assert (equal? '(a b c d) (topological-sort '((a b) (b c) (c d)) eq?)))
 (assert (equal? '(c d a b) (topological-sort '((a b) (c d)) eq?)))
 ;; (assert-error (topological-sort '((a b) (b a)) eq?))   ;; We don't support cycle detection yet
+
+;; Queues.
+
+;; These are tested extensively (and probably still not enough)
+;; because of the strange dealings with the front and end lists stored
+;; internally.  If we run into errors, add more regression tests here.
+
+(let ((q (make-queue)))
+  (assert (queue? q))
+  (assert (queue-empty? q))
+  (assert (= 0 (queue-length q)))
+  (assert (null? (queue->list q)))
+  (assert-error (queue-first q))
+  (assert-error (queue-last q))
+  (assert-error (queue-remove! q))
+
+  (queue-add! q 'foo)
+  (assert (eq? 'foo (queue-first q)))
+  (assert (eq? 'foo (queue-last q)))
+  (assert (not (queue-empty? q)))
+  (assert (= (queue-length q) 1))
+  (let ((l1 (queue->list q))
+        (l2 (queue->list q)))
+    (assert (equal? l1 '(foo)))
+    (assert (equal? l2 '(foo)))
+    (assert (not (eq? l1 l2)))          ; Do not share memory
+
+    (queue-add! q 'end)
+
+    (queue-push-back! q 'front)
+
+    (assert (equal? l1 '(foo))))      ; Does not share memory w/ queue
+  (assert (equal? (queue->list q) '(front foo end)))
+
+  (assert (eq? 'front (queue-remove! q)))
+  (assert (eq? 'foo (queue-first q)))
+  (assert (eq? 'end (queue-last q)))
+
+  (queue-push-back-list! q '(one two))
+  (assert (equal? (queue->list q) '(one two foo end)))
+  (assert (= 4 (queue-length q)))
+
+  (assert (eq? 'one (queue-remove! q)))
+  (assert (eq? 'two (queue-remove! q)))
+  (assert (= 2 (queue-length q)))
+  (assert (eq? 'foo (queue-first q)))
+  (assert (eq? 'end (queue-last q)))
+  (assert (not (queue-empty? q)))
+
+  (assert (eq? 'foo (queue-remove! q)))
+  (assert (eq? 'end (queue-first q)))
+  (assert (eq? 'end (queue-last q)))
+  (assert (= (queue-length q) 1))
+  (assert (not (queue-empty? q)))
+
+  (assert (eq? 'end (queue-remove! q)))
+  (assert (queue-empty? q))
+  (assert (= (queue-length q) 0))
+  (assert-error (queue-first q))
+  (assert-error (queue-last q))
+  (assert-error (queue-remove! q)))
+
+(let ((q (list->queue (list 'one 'two))))
+  (assert (queue? q))
+  (assert (not (queue-empty? q)))
+  (assert (= (queue-length q) 2))
+  (assert (eq? 'one (queue-first q)))
+  (assert (eq? 'two (queue-last q)))
+
+  (assert (eq? 'one (queue-remove! q)))
+  (assert (eq? 'two (queue-first q)))
+  (assert (eq? 'two (queue-last q)))
+  (assert (= (queue-length q) 1))
+  (assert (not (queue-empty? q)))
+
+  (assert (eq? 'two (queue-remove! q)))
+  (assert-error (queue-first q))
+  (assert-error (queue-last q))
+  (assert (= (queue-length q) 0))
+  (assert (queue-empty? q)))
+
+(let ((q (list->queue (list 'one))))
+  (assert (queue? q))
+  (assert (not (queue-empty? q)))
+  (assert (= (queue-length q) 1))
+  (assert (eq? 'one (queue-first q)))
+  (assert (eq? 'one (queue-last q)))
+
+  (queue-push-back! q 'zero)
+  (assert (eq? 'zero (queue-first q)))
+  (assert (eq? 'one (queue-last q)))
+
+  (queue-add! q 'two)
+  (assert (eq? 'zero (queue-first q)))
+  (assert (eq? 'two (queue-last q)))
+
+  (queue-add! q 'three)
+  (assert (eq? 'zero (queue-first q)))
+  (assert (eq? 'three (queue-last q)))
+  (assert (equal? '(zero one two three) (queue->list q)))
+
+  (assert (eq? 'zero (queue-remove! q)))
+  (assert (eq? 'one (queue-first q)))
+  (assert (eq? 'three (queue-last q)))
+  (assert (= (queue-length q) 3))
+  (assert (not (queue-empty? q)))
+
+  (assert (eq? 'one (queue-remove! q)))
+  (assert (eq? 'two (queue-remove! q)))
+  (assert (eq? 'three (queue-remove! q)))
+  (assert-error (queue-first q))
+  (assert-error (queue-last q))
+  (assert (= (queue-length q) 0))
+  (assert (queue-empty? q)))