56c907ce6b2c50924436e4e929b1fe5835dc2320
Peter Bex <peter@more-magic.net>
2017-01-17 20:57:11 +0100

    Do not use feature macros on FreeBSD.
    According to this post on the "freebsd-standards" mailing list, FreeBSD
    discourages using feature macros to enable features.  It takes a very
    strict approach and disables all other features, including "native"
    standard BSD functions when you enable a specific standard.  There is
    no way to re-enable the BSD functions (not even via _BSD_SOURCE).
    
    https://lists.freebsd.org/pipermail/freebsd-standards/2004-March/000474.html
    
    Signed-off-by: Evan Hanson <evhan@foldling.org>

--- chicken.h
+++ chicken.h
@@ -47,24 +47,29 @@
 # define __C99FEATURES__
 #endif
 
-#ifndef _XOPEN_SOURCE
-# define _XOPEN_SOURCE 700
-#endif
+/* Some OSes really dislike feature macros for standard levels */
+#ifdef C_USE_STD_FEATURE_MACROS
 
-#ifndef _BSD_SOURCE
-# define _BSD_SOURCE
-#endif
+# ifndef _XOPEN_SOURCE
+#  define _XOPEN_SOURCE 700
+# endif
 
-#ifndef _SVID_SOURCE
-# define _SVID_SOURCE
-#endif
+# ifndef _BSD_SOURCE
+#  define _BSD_SOURCE
+# endif
+
+# ifndef _SVID_SOURCE
+#  define _SVID_SOURCE
+# endif
 
 /*
  * glibc >= 2.20 synonym for _BSD_SOURCE & _SVID_SOURCE.
  */
-#ifndef _DEFAULT_SOURCE
-# define _DEFAULT_SOURCE
-#endif
+# ifndef _DEFAULT_SOURCE
+#  define _DEFAULT_SOURCE
+# endif
+
+#endif /* C_USE_STD_FEATURE_MACROS */
 
 /*
  * N.B. This file MUST not rely upon "chicken-config.h"
