0746cdf32e9a67b485aada16d7e6c30708c8aada
Evan Hanson <evhan@foldling.org>
2018-04-29 01:15:39 +0200

    Update bootstrap.sh to use CHICKEN 5.0.0pre7

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,31 +2,27 @@
 
 set -e
 
-# build 5.0.0pre6 development snapshot
+# build 5.0.0pre7 development snapshot
 
-mkdir -p boot/pre6
+mkdir -p boot/pre7
 cd boot
-wget http://code.call-cc.org/dev-snapshots/2018/03/12/chicken-5.0.0pre6.tar.gz
-tar -xzf chicken-5.0.0pre6.tar.gz
-cd chicken-5.0.0pre6
-make "$@" PREFIX="$(pwd)"/../pre6
-make "$@" PREFIX="$(pwd)"/../pre6 install
+wget http://code.call-cc.org/dev-snapshots/2018/04/29/chicken-5.0.0pre7.tar.gz
+tar -xzf chicken-5.0.0pre7.tar.gz
+cd chicken-5.0.0pre7
+make "$@" PREFIX="$(pwd)"/../pre7
+make "$@" PREFIX="$(pwd)"/../pre7 install
 cd ../..
 
-# build a boot-chicken from git head using pre6 chicken and
+# build a boot-chicken from git head using pre7 chicken and
 # then use that to build the real thing
-#
-# baf6363e535b26a67d0b9a7d71a93d8deb5de8c6 hardcodes
-# some import libraries and removes deprecated internals, so
-# a boot-chicken needs to be built.
 
 make "$@" spotless
-make "$@" CHICKEN="$(pwd)"/boot/pre6/bin/chicken boot-chicken
+make "$@" CHICKEN="$(pwd)"/boot/pre7/bin/chicken boot-chicken
 
-# remove pre6 installation and tarball
-rm -fr boot/pre6
-rm -fr boot/chicken-5.0.0pre6
-rm -f  boot/chicken-5.0.0pre6.tar.gz
+# remove pre7 installation and tarball
+rm -fr boot/pre7
+rm -fr boot/chicken-5.0.0pre7
+rm -f  boot/chicken-5.0.0pre7.tar.gz
 
 echo
 echo 'Now, build chicken by passing "CHICKEN=./chicken-boot" to make,'
