kdltr/chicken-core

commit d72c0b240a4992f8987575107e834bcccda1cdca

Allow `files' in the toplevel of egg description files
The toplevel `files' form is not actually used by chicken-install, but
by henrietta-cache.  henrietta-cache uses the `files' form to
determine what to cache.

Fixes #1511.

Signed-off-by: felix <felix@call-with-current-continuation.org>

chicken-install.scm
@@ -177,7 +177,7 @@
     (platform #t #f #f)
     (installed-files #t #f #f ,list?)
     (maintainer #t #f #f)
-    (files #f #f #f ,list?)
+    (files #t #f #f ,list?)
     (source #f #f #f)
     (csc-options #f #f #f)
     (link-options #f #f #f)

commit 2769143a08a646893fa0965af405b5adcc204b32

chicken-install: accept `distribution-files' egg property
Make chicken-install accept `distribution-files' in the toplevel of
egg description files.  chicken-install will actually not do anything
with `distribution-files' -- it will be handled by henrietta-cache.

Also revert d72c0b240a, which introduced a bug (`files' could only be
used at the toplevel).

Signed-off-by: felix <felix@call-with-current-continuation.org>

chicken-install.scm
@@ -177,7 +177,8 @@
     (platform #t #f #f)
     (installed-files #t #f #f ,list?)
     (maintainer #t #f #f)
-    (files #t #f #f ,list?)
+    (files #f #f #f ,list?)
+    (distribution-files #t #f #f ,list?) ;; handled by henrietta-cache
     (source #f #f #f)
     (csc-options #f #f #f)
     (link-options #f #f #f)

manual/Egg specification format
@@ -106,9 +106,9 @@ If the expression can not be satisfied, then installation of this
 egg will abort.
 les
 
-===== files
+===== distribution-files
 
- [egg property] (files FILE ...)
+ [egg property] (distribution-files FILE ...)
 
 List of files required for the installation of the egg.  This
 form is not handled by chicken-install, but by henrietta-cache to
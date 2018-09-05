2769143a08a646893fa0965af405b5adcc204b32
Mario Domenech Goulart <mario@parenteses.org>
2018-09-05 09:46:37 +0200

    chicken-install: accept `distribution-files' egg property
    Make chicken-install accept `distribution-files' in the toplevel of
    egg description files.  chicken-install will actually not do anything
    with `distribution-files' -- it will be handled by henrietta-cache.
    
    Also revert d72c0b240a, which introduced a bug (`files' could only be
    used at the toplevel).
    
    Signed-off-by: felix <felix@call-with-current-continuation.org>

--- chicken-install.scm
+++ chicken-install.scm
@@ -177,7 +177,8 @@
     (platform #t #f #f)
     (installed-files #t #f #f ,list?)
     (maintainer #t #f #f)
-    (files #t #f #f ,list?)
+    (files #f #f #f ,list?)
+    (distribution-files #t #f #f ,list?) ;; handled by henrietta-cache
     (source #f #f #f)
     (csc-options #f #f #f)
     (link-options #f #f #f)
