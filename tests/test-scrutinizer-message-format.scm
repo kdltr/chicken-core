9834b1a6b56419fcc800b80d557791a74f661709
megane <meganeka@gmail.com>
2019-03-30 09:26:59 +0200

    Make scrutinizer message format test suite more comprehensive
    Signed-off-by: Evan Hanson <evhan@foldling.org>
    Signed-off-by: Peter Bex <peter@more-magic.net>

--- tests/test-scrutinizer-message-format.scm
+++ tests/test-scrutinizer-message-format.scm
@@ -1,4 +1,5 @@
 (import (chicken time))
+
 (: deprecated-foo deprecated)
 (define deprecated-foo 1)
 (: deprecated-foo2 (deprecated foo))
@@ -23,15 +24,13 @@
 
 (set! foo 1)
 
+;; These have special cases
 (define (list-ref-negative-index) (list-ref '() -1))
 (define (list-ref-out-of-range) (list-ref '() 1))
-(define (append-invalid-last-arg) (scheme#append (list 1) 1)) ;; TODO: doesn't work
+(define (append-invalid-arg) (append 1 (list 1)))
 (define (vector-ref-out-of-range) (vector-ref (vector) -1))
-(define (zero-values-for-let) (let ((a (values))) a))
-(define (multiple-values-for-let) (let ((a (values 1 2))) a))
-(define (zero-values-for-conditional) (if (values) 1))
-(define (multiple-values-for-conditional) (if (values 1 2) 1))
 
+;; This is disabled because fail-compiler-typecase is a fatal warning
 ;; (define (fail-compiler-typecase) (compiler-typecase 1 (symbol 1) (list 2)))
 
 (module
@@ -51,9 +50,11 @@
    (define (local-bar)
      (define (r-proc-call-argument-count-mismatch) (cons '()))
      (define (r-proc-call-argument-type-mismatch) (length 'symbol))
-     (define (r-proc-call-argument-value-count) (list (cpu-time)) (vector (values)) ((values)))
+     (define (r-proc-call-argument-value-count) (list (cpu-time)) (vector (values)))
      (define (r-cond-branch-value-count-mismatch) (if (the * 1) 1 (cpu-time)))
-     (define (r-invalid-called-procedure-type) (1 2))
+     (define (r-invalid-called-procedure-type)
+       (define (variable) (foo2 2))
+       (define (non-variable) (1 2)))
      (define (r-pred-call-always-true) (list? '()))
      (define (r-pred-call-always-false) (symbol? 1))
      (define (r-cond-test-always-true) (if (length '()) 1))
@@ -64,14 +65,24 @@
      (define (r-toplevel-var-assignment-type-mismatch) (set! foo2 1))
      (define (r-deprecated-identifier) (list deprecated-foo) (vector deprecated-foo2))
 
+     (define (r-let-value-count-invalid)
+       (define (zero-values-for-let) (let ((a (values))) a))
+       (define (too-many-values-for-let) (let ((a (values 1 2))) a)))
+     (define (r-conditional-value-count-invalid)
+       (define (zero-values-for-conditional) (if (values) 1))
+       (define (too-many-values-for-conditional) (if (values (the * 1) 2) 1)))
+     (define (r-assignment-value-count-invalid)
+       (define (zero-values-for-assignment) (set! foo (values)))
+       (define (too-many-values-for-assignment) (set! foo (values #t 2))))
+
+     ;; These have special cases
      (define (list-ref-negative-index) (list-ref '() -1))
      (define (list-ref-out-of-range) (list-ref '() 1))
-     (define (append-invalid-last-arg) (scheme#append (list 1) 1)) ;; TODO: doesn't work
+     (define (append-invalid-arg) (append 1 (list 1)))
      (define (vector-ref-out-of-range) (vector-ref (vector) -1))
-     (define (zero-values-for-let) (let ((a (values))) a))
-     (define (multiple-values-for-let) (let ((a (values 1 2))) a))
-     (define (zero-values-for-conditional) (if (values) 1))
-     (define (multiple-values-for-conditional) (if (values 1 2) 1))
+
+     (define (r-cond-test-always-true-with-pred) (if (symbol? 'symbol) 1))
+     (define (r-cond-test-always-false-with-pred) (if (symbol? 1) 1))
 
      (define (fail-compiler-typecase) (compiler-typecase 1 (symbol 1) (list 2)))
      )))
