kdltr/chicken-core

commit d902e1cfd6d8d9ca8841bc83f30e8547c9f32c2f

Introduce XDG directories
This patch adds to new procedures to chicken.platform:
 - system-config-directory
 - system-cache-directory

These two procedures try their best at finding what’s the user prefered
location for config and cache files. They work on POSIX systems and
windows.

This change also makes csi (to find csirc) and chicken-install (for its
cache, and a new user-defined setup.defauts) use these two new procedures.

Fixes ticket #1455

Signed-off-by: Peter Bex <peter@more-magic.net>

chicken-install.scm
@@ -288,8 +288,12 @@
 ;; load defaults file ("setup.defaults")
 
 (define (load-defaults)
-  (let ((deff (or user-defaults
-                  (make-pathname host-sharedir +defaults-file+))))
+  (let* ((cfg-dir (system-config-directory))
+         (user-file (and cfg-dir (make-pathname (list cfg-dir "chicken")
+                                                +defaults-file+)))
+         (deff (or user-defaults
+                   (and (file-exists? user-file) user-file)
+                   (make-pathname host-sharedir +defaults-file+))))
       (define (broken x)
 	(error "invalid entry in defaults file" deff x))
       (cond ((not (file-exists? deff)) '())

chicken-install.mdoc
@@ -120,6 +120,23 @@ The command to execute when using
 flag in command. If not provided, defaults to
 .Xr sudo 8 .
 .El
+.Sh FILES
+.Bl -tag -width 4n
+.It Pa $XDG_CONFIG_HOME/chicken/setup.defaults
+User specific setup.defaults file. (
+.Ev $XDG_CONFIG_HOME
+defaults to
+.Pa $HOME/.config
+)
+.It Pa $prefix/share/chicken/setup.default
+System-wide setup.defaults file.
+.It Pa $XDG_CACHE_HOME/chicken-install/
+Default directory for cached eggs. (
+.Ev $XDG_CACHE_HOME
+defaults to
+.Pa $HOME/.cache
+)
+.El
 .Sh EXIT STATUS
 .Ex -std
 .Sh EXAMPLES

chicken-status.mdoc
@@ -81,6 +81,17 @@ package library path selected during configuration
 .It Ev CHICKEN_REPOSITORY_PATH
 One or more directories holding extension libraries. Defaults to the
 installation repository.
+.It Ev CHICKEN_EGG_CACHE
+Location where eggs are retrieved and built.
+.El
+.Sh FILES
+.Bl -tag -width 4n
+.It Pa $XDG_CACHE_HOME/chicken-install/
+Default directory for cached eggs. (
+.Ev $XDG_CACHE_HOME
+defaults to
+.Pa $HOME/.cache
+)
 .El
 .Sh EXIT STATUS
 .Ex -std

csi.mdoc
@@ -120,6 +120,17 @@ for include files, separated by
 .Sq \&;
 characters.
 .El
+.Sh FILES
+.Bl -tag -width 4n
+.It Pa $XDG_CONFIG_HOME/chicken/csirc
+Scheme file loaded on startup. (
+.Ev $XDG_CONFIG_HOME
+defaults to
+.Pa $HOME/.config
+)
+.It Pa $HOME/.csirc
+Scheme file loaded on startup if the previous one doesn’t exists.
+.El
 .Sh EXIT STATUS
 .Ex -std
 .Sh SEE ALSO

csi.scm
@@ -56,6 +56,7 @@ EOF
 	chicken.io
 	chicken.keyword
 	chicken.load
+	chicken.pathname
 	chicken.platform
 	chicken.port
 	chicken.pretty-print
@@ -72,7 +73,7 @@ EOF
 
 ;;; Parameters:
 
-(define-constant init-file ".csirc")
+(define-constant init-file "csirc")
 
 (set! ##sys#repl-print-length-limit 2048)
 (set! ##sys#features (cons #:csi ##sys#features))
@@ -1012,11 +1013,16 @@ EOF
 			  (cons (cadr p) (loop (cddr p)))) ) ]
 		[else '()] ) ) )
       (define (loadinit)
-	(and-let* ((home (get-environment-variable "HOME"))
-		   ((not (string=? home ""))))
-	  (let ((fn (string-append (chop-separator home) "/" init-file)))
-	    (when (file-exists? fn)
-		  (load fn) ) ) ) )
+        (let* ((sys-dir (system-config-directory))
+               (cfg-fn (and sys-dir (make-pathname (list sys-dir "chicken")
+                                                   init-file)))
+               (home (get-environment-variable "HOME"))
+               (home-fn (and home (not (string=? home ""))
+                             (make-pathname home (string-append "." init-file)))))
+          (cond ((and cfg-fn (file-exists? cfg-fn))
+                 (load cfg-fn))
+                ((and home-fn (file-exists? home-fn))
+                 (load home-fn) ) ) ) )
       (define (evalstring str #!optional (rec (lambda _ (void))))
 	(let ((in (open-input-string str)))
 	  (do ([x (read in) (read in)])

egg-environment.scm
@@ -110,13 +110,11 @@ EOF
       (or (get-environment-variable "CHICKEN_INSTALL_REPOSITORY")
           host-repo)))
 
-(define (probe-dir dir)
+(define (probe-dir dir)           
   (and dir (directory-exists? dir) dir))
 
 (define cache-directory
   (or (get-environment-variable "CHICKEN_EGG_CACHE")
-      (make-pathname (list (or (probe-dir (get-environment-variable "HOME"))
-                               (probe-dir (get-environment-variable "USERPROFILE"))
-                               (current-directory))
-                           ".chicken-install")
-                     "cache")))
+      (make-pathname (or (system-cache-directory)
+                         (current-directory))
+                     "chicken-install")))

library.scm
@@ -6403,6 +6403,7 @@ static C_word C_fcall C_setenv(C_word x, C_word y) {
      repository-path installation-repository
      register-feature! unregister-feature!
      software-type software-version return-to-host
+     system-config-directory system-cache-directory
      )
 
 (import scheme)
@@ -6599,5 +6600,20 @@ static C_word C_fcall C_setenv(C_word x, C_word y) {
 
 (define return-to-host
   (##core#primitive "C_return_to_host"))
+
+(define (system-config-directory)
+  (or (get-environment-variable "XDG_CONFIG_HOME")
+      (if ##sys#windows-platform
+          (get-environment-variable "APPDATA")
+          (let ((home (get-environment-variable "HOME")))
+            (and home (string-append home "/.config"))))))
+
+(define (system-cache-directory)
+  (or (get-environment-variable "XDG_CACHE_HOME")
+      (if ##sys#windows-platform
+          (or (get-environment-variable "LOCALAPPDATA")
+              (get-environment-variable "APPDATA"))
+          (let ((home (get-environment-variable "HOME")))
+            (and home (string-append home "/.cache"))))))
 
 ) ; chicken.platform

manual/Module (chicken platform)
@@ -60,6 +60,27 @@ Contains the name of the directory where extensions are installed
 linked at runtime.)
 
 
+=== system-config-directory
+
+<procedure>(system-config-directory</procedure>
+
+Returns the location of the directory for configuration files.  This
+obeys the XDG specification, so when the {{XDG_CONFIG_HOME}}
+environment variable is set, its value is used.  When it's not set, it
+will default to {{$HOME/.config}} on UNIX and {{$APPDATA}} on Windows.
+
+
+=== system-cache-directory
+
+<procedure>(system-cache-directory)</procedure>
+
+Returns the location of the directory for caches.  This obeys the XDG
+specification, so when the {{XDG_CACHE_HOME}} environment variable is
+set, its value is used.  When it's not set, it will default to
+{{$HOME/.cache}} on UNIX and {{$LOCALAPPDATA}} or {{$APPDATA}} on
+Windows.
+
+
 === Machine information
 
 These procedures return information about the type of CPU/architecture

commit 0e98bc4286d608703d848cfdc617b58d511fc369

Fix chicken-install error caused by non-string argument to `file-exists?'
Commit d902e1cf introduced a potential error when chicken-install tries
to load its defaults file, since `file-exists?' might be called with a
non-string argument when `user-file' is false.

The fix is simple, we just move the file existence test after the
variable test, since `file-exists?' returns the filename anyway.

chicken-install.scm
@@ -292,7 +292,7 @@
          (user-file (and cfg-dir (make-pathname (list cfg-dir "chicken")
                                                 +defaults-file+)))
          (deff (or user-defaults
-                   (and (file-exists? user-file) user-file)
+                   (and user-file (file-exists? user-file))
                    (make-pathname host-sharedir +defaults-file+))))
       (define (broken x)
 	(error "invalid entry in defaults file" deff x))