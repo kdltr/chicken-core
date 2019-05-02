687f5070c98e6b7d0baafd6adbd8332abc0bd495
Peter Bex <peter@more-magic.net>
2019-05-02 18:34:43 +0200

    Update bootstrap script to use 5.0.2 dev snapshot

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# build 5.0.1 development snapshot
+# build 5.0.2 development snapshot
 
 mkdir -p boot/snapshot
 cd boot
-wget https://code.call-cc.org/dev-snapshots/2019/01/12/chicken-5.0.1.tar.gz
-tar -xzf chicken-5.0.1.tar.gz
-cd chicken-5.0.1
+wget https://code.call-cc.org/dev-snapshots/2019/05/02/chicken-5.0.2.tar.gz
+tar -xzf chicken-5.0.2.tar.gz
+cd chicken-5.0.2
 make "$@" PREFIX="$(pwd)"/../snapshot
 make "$@" PREFIX="$(pwd)"/../snapshot install
 cd ../..
@@ -21,8 +21,8 @@ make "$@" CHICKEN="$(pwd)"/boot/snapshot/bin/chicken boot-chicken
 
 # remove snapshot installation and tarball
 rm -fr boot/snapshot
-rm -fr boot/chicken-5.0.1
-rm -f  boot/chicken-5.0.1.tar.gz
+rm -fr boot/chicken-5.0.2
+rm -f  boot/chicken-5.0.2.tar.gz
 
 echo
 echo 'Now, build chicken by passing "CHICKEN=./chicken-boot" to make,'
