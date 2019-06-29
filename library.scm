kdltr/chicken-core

commit f7f3923319f8e893610409725316bf7e49a491be

Fix csi's "report" feature and add guard for `keyword-style' parameter
The interpreter's "report" command (,r) was a casualty of db2608b1f,
which causes `symbol->string' to fail when passed a keyword.

Update that code to use `keyword->string' and add a guard procedure to
the keyword-style parameter to ensure that it is always a keyword.

Signed-off-by: Peter Bex <peter@more-magic.net>

library.scm
@@ -3677,9 +3677,11 @@ EOF
   (##sys#read-error port "invalid `#...' read syntax" n) )
 
 (set! chicken.base#case-sensitive (make-parameter #t))
-(set! chicken.base#keyword-style (make-parameter #:suffix))
 (set! chicken.base#parentheses-synonyms (make-parameter #t))
 (set! chicken.base#symbol-escape (make-parameter #t))
+
+(set! chicken.base#keyword-style
+  (make-parameter #:suffix (lambda (x) (##sys#check-keyword x 'keyword-style) x)))
 
 (define ##sys#current-read-table (make-parameter (##sys#make-structure 'read-table #f #f #f)))
 

csi.scm
@@ -508,7 +508,7 @@ EOF
                     (installation-repository)
 		    (repository-path)
 		    ##sys#include-pathnames
-		    (symbol->string (keyword-style))
+		    (keyword->string (keyword-style))
 		    (shorten (vector-ref sinfo 0))
 		    (shorten (vector-ref sinfo 1))
 		    (vector-ref sinfo 2)

commit 8613c505152d4dac210a473f2494c7446951d93a

Allow #f as keyword-style, unbreaks the tests
Was a regression caused by f7f3923319f8e893610409725316bf7e49a491be

library.scm
@@ -3681,7 +3681,7 @@ EOF
 (set! chicken.base#symbol-escape (make-parameter #t))
 
 (set! chicken.base#keyword-style
-  (make-parameter #:suffix (lambda (x) (##sys#check-keyword x 'keyword-style) x)))
+  (make-parameter #:suffix (lambda (x) (when x (##sys#check-keyword x 'keyword-style)) x)))
 
 (define ##sys#current-read-table (make-parameter (##sys#make-structure 'read-table #f #f #f)))
 