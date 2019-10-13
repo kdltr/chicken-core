4a802e74bac51f468defe35807ae0ea394d57c4e
megane <meganeka@gmail.com>
2019-10-13 16:25:38 +0300

    * tests/inline-unroll.scm: Use trivial case from the issue page

--- tests/inline-unroll.scm
+++ tests/inline-unroll.scm
@@ -1,15 +1,8 @@
 ;; trivial test for catching runaway inlining (#1648), by
 ;; megane:
 
-(module uri-generic
-        (uri-relative-from)
-
-        (import scheme)
-
-        (define (uri-relative-from uabs base)
-          (dif-segs-from uabs base))
-
-        (define (dif-segs-from sabs base)
-          (if (null? base)
-              sabs
-              (dif-segs-from sabs base))))
+(module
+ m1 (foo)
+ (import scheme)
+ (define (foo x) (bar x))
+ (define (bar x) (if (null? x) x (bar (cdr x)))))
